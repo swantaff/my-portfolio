decafe17b616e51dab45bcdf878c6719b608711b
Mark Jones <marks.jones@yahoo.co.uk>
2019-02-24 21:04:36 +0000

    corrected type in codepipeline

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -30,6 +30,7 @@ def lambda_handler(event, context):
         s3 = boto3.resource('s3')
         
         portfolio_bucket = s3.Bucket('portfolio.markjones.info')
+        
         build_bucket = s3.Bucket(location["bucketName"])
         
         portfolio_zip = StringIO.StringIO()
