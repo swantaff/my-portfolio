15e0a478954808c7ce5549609556161b1e9ce710
swantaff <marks.jones@yahoo.co.uk>
2019-02-25 15:18:48 +0000

    update deployLambda script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
 
         if job:
             for artifact in job["data"] [inputArtifacts]:
-                if artifact["name"] == "MyAppBuild":
+                if artifact["name"] == "BuildArtifact":
                     location = artifact["location"]["s3Location"]
         
         print("Building portfolio from") + str(location)
